d83ea1b29a4b5a6fa43768c2a31449aed66d1ceb
Alexey Chuprin <a.chuprin@capjack.ru>
2025-06-27 01:34:35 +0300

    Improve DurationDeserializer

--- src/main/kotlin/org/shypl/tool/app/config/DurationDeserializer.kt
+++ src/main/kotlin/org/shypl/tool/app/config/DurationDeserializer.kt
@@ -8,14 +8,14 @@ import java.time.Duration
 
 private val regex = Regex("\\s*(?:(\\d+)d)?\\s*(?:(\\d+)h)?\\s*(?:(\\d+)m)?\\s*(?:(\\d+)s)?\\s*")
 
-private fun stringToSeconds(string: String): Int {
+private fun stringToDuration(string: String): Duration {
 	val e = regex.matchEntire(string)
 	if (e != null) {
-		var v = 0
-		e.groups[1]?.also { v += it.value.toInt() * 60 * 60 * 24 }
-		e.groups[2]?.also { v += it.value.toInt() * 60 * 60 }
-		e.groups[3]?.also { v += it.value.toInt() * 60 }
-		e.groups[4]?.also { v += it.value.toInt() }
+		var v = Duration.ZERO
+		e.groups[1]?.also { v = v.plusDays(it.value.toLong()) }
+		e.groups[2]?.also { v = v.plusHours(it.value.toLong()) }
+		e.groups[3]?.also { v = v.plusMinutes(it.value.toLong()) }
+		e.groups[4]?.also { v = v.plusSeconds(it.value.toLong()) }
 		return v
 	}
 	throw IllegalArgumentException()
@@ -23,7 +23,7 @@ private fun stringToSeconds(string: String): Int {
 
 class DurationDeserializer : JsonDeserializer<Duration>() {
 	override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Duration {
-		return Duration.ofSeconds(stringToSeconds(p.valueAsString).toLong())
+		return stringToDuration(p.valueAsString)
 	}
 }
 
@@ -31,7 +31,11 @@ class TimeStringToSecondsIntDeserializationProblemHandler : DeserializationProbl
 	
 	override fun handleWeirdStringValue(ctxt: DeserializationContext, targetType: Class<*>, valueToConvert: String, failureMsg: String): Any {
 		if (targetType == Int::class.java || targetType == Long::class.java) {
-			return stringToSeconds(valueToConvert)
+			val duration = stringToDuration(valueToConvert).seconds
+			if (targetType == Int::class.java) {
+				return duration.toInt()
+			}
+			return duration
 		}
 		
 		return super.handleWeirdStringValue(ctxt, targetType, valueToConvert, failureMsg)
